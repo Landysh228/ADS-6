3a0b953d70d8b48aef6b3d7470da8a66f5f2bd12
Landysh <walfa45milana@mail.ru>
2026-05-06 19:55:06 +0300

    Чжен-25ИСТ41.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,54 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+template<typename T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node * next;
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+  Node* head;
+ public:
+  TPQueue() : head(nullptr) {}
+  ~TPQueue() {
+    while (head) {
+      Node* tmp = head;
+      head = head -> next;
+      delete tmp;
+    }
+  }
+  TPQueue(const TPQueue&) = delete;
+  TPQueue& operator = (const TPQueue&) = delete;
+  void push(const T& value) {
+    Node * newNode = new Node(value);
+    if (!head || value.prior > head->data.prior) {
+      newNode -> next = head;
+      head = newNode;
+    } else {
+      Node * curt = head;
+      while (curt -> next && curt -> next -> data.prior >= value.prior) {
+        curt = curt -> next;
+      }
+      newNode -> next = curt -> next;
+      curt -> next = newNode;
+    }
+  }
+  T pop() {
+    if (!head) {
+      throw "Queue is empty";
+    }
+    Node * tmp = head;
+    T rslt = head -> data;
+    head = head -> next;
+    delete tmp;
+    return rslt;
+  }
+};
+struct SYM {
+  char ch;
+  int prior;
+};
+#endif  // INCLUDE_TPQUEUE_H_
