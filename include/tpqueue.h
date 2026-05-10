3888cd4c4ddf502cd0d25050422f4cf2e1901892
Landysh <walfa45milana@mail.ru>
2026-05-10 11:59:39 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,66 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T vall;
+    Node* next;
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() {
+    head = nullptr;
+  }
+
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* tmp = head;
+      head = head->next;
+      delete tmp;
+    }
+  }
+
+  void push(T x) {
+    Node* n = new Node;
+    n->vall = x;
+    n->next = nullptr;
+
+    if (head == nullptr || x.prior > head->vall.prior) {
+      n->next = head;
+      head = n;
+      return;
+    }
+
+    Node* cur = head;
+
+    while (cur->next != nullptr &&
+           cur->next->vall.prior >= x.prior) {
+      cur = cur->next;
+    }
+
+    n->next = cur->next;
+    cur->next = n;
+  }
+
+  T pop() {
+    if (head == nullptr) {
+      return T{};
+    }
+
+    Node* tmp = head;
+    T res = head->vall;
+
+    head = head->next;
+    delete tmp;
+
+    return res;
+  }
+
+  bool isEmpty() {
+    return head == nullptr;
+  }
 };
 
 struct SYM {
